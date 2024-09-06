a37ac25d7cd73c62b3a17cf6f983882d8af0de37
Felix McDermott <you@example.com>
2024-09-06 13:47:35 +0100

    ocr button added

--- frontend/src/appconfig.js
+++ frontend/src/appconfig.js
@@ -3,9 +3,14 @@ const config = {
     "https://bjssacademyhackday-a8fqfwapbxfpa4b5.uksouth-01.azurewebsites.net",
   teamName: "atari",
 
+
   imageServiceUrl: function () {
     return `${config.imageServiceBaseUrl}/IL/teams/${config.teamName}/files`;
   },
+
+  textServiceUrl: function () {
+    return ``;
+  }
 };
 
 export default config;
